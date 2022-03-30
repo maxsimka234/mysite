37507e0c6b3d6cc39d6e887a79b5367ad08c321a
maxsimka <max.boreychuk@gmail.com>
2022-03-30 11:34:22 +0300

    upd home page design

--- forum/b/views.py
+++ forum/b/views.py
@@ -49,7 +49,7 @@ def add_coment(request, self_post_id):
                                                  comment_text=form.cleaned_data["comment_text"] , comment_pub_date=datetime.datetime.now() )
                 return HttpResponseRedirect(reverse("post_page",args=str(self_post_id)))
             else:
-                return HttpResponseRedirect(reverse("post_page",args=str(self_post_id)))          
+                return HttpResponseRedirect(reverse("post_page",args=str(self_post_id)))       
     else: 
         return HttpResponse(request)    
         
